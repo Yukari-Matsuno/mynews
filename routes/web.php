f909aa557c3c73c4c5272edb785accc6f5e84ce6
”yukari <mareie.m.1005@gmail.com>
2020-02-07 17:58:08 +0900

    PHP/Laravel 12までやった

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,18 @@ Route::group(['prefix' => 'admin'], function() {
 // Route::get('XXX', 'AAAcontroller@bbb');
 
 //PHP/Laravel 09 Routingについて理解する 課題4.
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
+Route::get('admin/profile/create', 'Admin\ProfileController@add') -> middleware('auth');;
 
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit') -> middleware('auth');;
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create', 'Admin\NewsController@add') -> middleware('auth');
+});
 
 
 
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
